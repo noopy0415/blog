09a4bf2aa37265a18d5a3b068b64395b992ea188
noopy0415 <noopy0415@gmail.com>
2018-05-06 20:52:56 +0900

    chemicalsコchemicalsコントローラーとplacesコントローラーを作成

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,37 @@
 class PlacesController < ApplicationController
   def index
+    @places = Place.all
   end
 
   def new
+    @place = Place.new
   end
 
   def create
-  end
-
-  def show
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @place = Place.find(params[:id])
   end
 
   def update
+    @place = place.find(params[:id])
+    if @place.update(place_params)
+      redirect_to places_path
+    else
+      render :edit
+    end
   end
 
-  def destroy
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :area, :lat, :lng, :info)
   end
 end
