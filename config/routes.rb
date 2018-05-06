5fcc45f77c5c26070c67adfa076b216c853d8492
noopy0415 <noopy0415@gmail.com>
2018-05-06 22:31:34 +0900

    produceコントローラー作成＆header作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,35 +2,7 @@ Rails.application.routes.draw do
   resources :works
   resources :chemicals
   resources :places
-
-  get 'produces/index'
-
-  get 'produces/new'
-
-  get 'produces/create'
-
-  get 'produces/show'
-
-  get 'produces/edit'
-
-  get 'produces/update'
-
-  get 'produces/destroy'
-
-  get 'produce/index'
-
-  get 'produce/new'
-
-  get 'produce/create'
-
-  get 'produce/show'
-
-  get 'produce/edit'
-
-  get 'produce/update'
-
-  get 'produce/destroy'
-
+  resources :produces
   devise_for :users
   resources :tasks
   root "tasks#index"
